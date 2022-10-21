9b1ffa8074869f9ea4c0cc9d1d761d0d7fe2c08f
xchangrey <geroychristian@gmail.com>
2022-10-21 15:01:49 +0800

    Fixes search functionality for posts

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,10 +4,10 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    public static void findByTitleContaining(String title){
-        return List<Post>;
-    }
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
 }
